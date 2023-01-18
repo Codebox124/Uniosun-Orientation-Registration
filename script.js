48f8d4ed41697f789b28f1fe7ef88b1664bdf32a
sheriff oladimeji <dimejiademola5@gmail.com>
2023-01-18 23:49:58 +0100

    fixed sum bugs

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const form = document.querySelector("#form");
 const name = document.querySelector("#name");
 const regNo = document.querySelector("#reg-number");
-const  phoneNo= document.querySelector("#phone-number");
-const  email = document.querySelector('#email')
+const phoneNo = document.querySelector("#phone-number");
+const email = document.querySelector("#email");
 
 function checkInput() {
   const nameValue = name.value.trim();
   const emailValue = email.value.trim();
-  const regNoValue= regNo.value.trim();
- const phoneNoValue = phoneNo.value.trim();
- 
-//  validation for name
+  const regNoValue = regNo.value.trim();
+  const phoneNoValue = phoneNo.value.trim();
+
+  //  validation for name
   if (nameValue === "") {
     errorMsg(name, "Name cannot be empty");
   } else if (nameValue.length < 3) {
@@ -18,37 +18,30 @@ function checkInput() {
   } else {
     success(name);
   }
-// email validation
-if(emailValue === ''){
-  errorMsg(email, "Email cannot be empty")
-}
-else if(!isEmail(emailValue)){
-  errorMsg(email, "Email is not valid")
-}
-else{
-  success(email)
-}
-// validation for RegNo
-if(regNoValue === ''){
-  errorMsg(regNo, "Reg No can't be empty")
-}
-else if(regNoValue.length < 14 || regNoValue.length >14 ){
-  errorMsg(regNo, "Not a Valid Registration Number")
-}
-else{
-  success(regNo)
-}
-// validation for phone No
-if(phoneNoValue === ''){
-  errorMsg(phoneNo, "Phone Number can't be empty")
-}
-else if (isNaN(phoneNoValue)){
-  errorMsg(phoneNo, "Not a valid Phone Number")
-}
-else{
-  success(phoneNo)
-}
-
+  // email validation
+  if (emailValue === "") {
+    errorMsg(email, "Email cannot be empty");
+  } else if (!isEmail(emailValue)) {
+    errorMsg(email, "Email is not valid");
+  } else {
+    success(email);
+  }
+  // validation for RegNo
+  if (regNoValue === "") {
+    errorMsg(regNo, "Reg No can't be empty");
+  } else if (regNoValue.length < 14 || regNoValue.length > 14) {
+    errorMsg(regNo, "Not a Valid Registration Number");
+  } else {
+    success(regNo);
+  }
+  // validation for phone No
+  if (phoneNoValue === "") {
+    errorMsg(phoneNo, "Phone Number can't be empty");
+  } else if (isNaN(phoneNoValue)) {
+    errorMsg(phoneNo, "Not a valid Phone Number");
+  } else {
+    success(phoneNo);
+  }
 }
 function errorMsg(input, value) {
   const formControl = input.parentElement;
@@ -58,7 +51,7 @@ function errorMsg(input, value) {
 }
 function success(input) {
   const formControl = input.parentElement;
-  formControl.className = "form-control success"
+  formControl.className = "form-control success";
 }
 function isEmail(email) {
   return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
